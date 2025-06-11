5975eb810dee9c4ebbd7467c66fb11dba3392288
Yin Yu <yzy5368@gmail.com>
2025-06-10 21:53:06 -0700

    rename

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -4,10 +4,14 @@ from .checkpoint import (
     save_checkpoint,
     load_checkpoint,
 )
+from .prediction import predict_continuous, predict_continuous_simple
+
 __all__ = [
     "generate_weak_weights",
     "plot_trajectory",
     "plot_hist",
     "save_checkpoint",
     "load_checkpoint",
+    "predict_continuous",
+    "predict_continuous_simple",
 ]
