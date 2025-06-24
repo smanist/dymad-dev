c2ff263aa68a2d744110b86e2d1389acfc385464
Daning Huang <huangdn.nuaa@gmail.com>
2025-06-24 09:28:08 -0400

    Ordering in init in train

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,13 +1,15 @@
 from .trainer_base import TrainerBase
-from .node_trainer import NODETrainer
 from .gkbf_trainer import GKBFTrainer
+from .kbf_trainer import KBFTrainer
 from .lstm_trainer import LSTMTrainer
+from .node_trainer import NODETrainer
 from .weak_form_trainer import WeakFormTrainer
 
 __all__ = [
     "TrainerBase",
-    "NODETrainer",
     "GKBFTrainer",
+    "KBFTrainer",
     "LSTMTrainer",
+    "NODETrainer",
     "WeakFormTrainer"
 ]
