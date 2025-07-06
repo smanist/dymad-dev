f1beb8fddd96a2fca32c3fd4fd2b0d6e1ee37062
Daning Huang <huangdn.nuaa@gmail.com>
2025-07-06 16:06:08 -0400

    Added infrastructure for RTD.

---
+++ docs/conf.py
@@ -0,0 +1,56 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+import importlib.metadata, sys, os
+sys.path.insert(0, os.path.abspath(".."))
+
+project = 'dymad'
+copyright = '2025, APUS Lab @ PSU'
+author = 'APUS Lab @ PSU'
+
+version = '0.1.0'
+release = importlib.metadata.version("dymad")
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.doctest',
+    # "myst_parser",   # Loaded by myst_nb
+    "myst_nb",
+    "sphinx_autodoc_typehints",
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+autosummary_generate = True
+autodoc_default_options = {
+    "members": True,
+    "undoc-members": True,
+    "show-inheritance": True,
+}
+
+nb_execution_mode = "off"         # "auto", "off", "force", or "cache"
+# nb_execution_cache_path = ".jupyter_cache"  # default; nice to keep env tidy
+# nb_execution_timeout = 180        # seconds per cell (tune as needed)
+
+myst_enable_extensions = [
+    "amsmath",
+    "dollarmath"
+]
+myst_dmath_allow_labels = True
+myst_dmath_double_inline = True
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,15 @@
+.. dymad documentation master file
+
+DyMAD documentation
+=======================
+
+This is the root.
+
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+..    getting_started
+..    examples
+..    api_ref
