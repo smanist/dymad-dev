39ea18e87e9b732a3d4eb7b994b927f9ac624e2c
Daning Huang <huangdn.nuaa@gmail.com>
2025-07-14 22:02:18 -0400

    Updated lti/ltd scripts for recent changes.

--- test/linear_time_invariant/lti_train.py
+++ test/linear_time_invariant/lti_train.py
@@ -6,7 +6,7 @@ import torch
 from dymad.data import DynData
 from dymad.models import LDM, KBF
 from dymad.training import WeakFormTrainer, NODETrainer
-from dymad.utils import load_model, plot_trajectory, setup_logging, TrajectorySampler
+from dymad.utils import load_model, plot_summary, plot_trajectory, setup_logging, TrajectorySampler
 
 B = 128
 N = 501
@@ -38,11 +38,11 @@ config_gau = {
             "dt":   0.2,
             "mode": "zoh"}}}
 
-MDL, mdl = LDM, 'ldm'
-# MDL, mdl = KBF, 'kbf'
+# MDL, mdl = LDM, 'ldm'
+MDL, mdl = KBF, 'kbf'
 
 ifdat = 0
-iftrn = 1
+iftrn = 0
 ifplt = 1
 ifprd = 1
 
@@ -59,7 +59,7 @@ if iftrn:
         {"model" : KBF, "trainer": NODETrainer,     "config": 'lti_kbf_node.yaml'}
     ]
 
-    for _i in [0, 1]:
+    for _i in [0, 1, 2, 3]:
         Model = cases[_i]['model']
         Trainer = cases[_i]['trainer']
         config_path = cases[_i]['config']
@@ -70,31 +70,10 @@ if iftrn:
         trainer.train()
 
 if ifplt:
-    sum_wf = np.load(f'results/lti_{mdl}_wf_summary.npz')
-    sum_nd = np.load(f'results/lti_{mdl}_node_summary.npz')
+    npz_files = [f'results/lti_{mdl}_node_summary.npz', f'results/lti_{mdl}_wf_summary.npz']
+    npzs = plot_summary(npz_files, labels = [f'{mdl}/NODE', f'{mdl}/WF'], ifclose=False)
 
-    e_loss_wf, h_loss_wf = sum_wf['epoch_loss'], sum_wf['losses']
-    e_loss_nd, h_loss_nd = sum_nd['epoch_loss'], sum_nd['losses']
-    e_rmse_wf, h_rmse_wf = sum_wf['epoch_rmse'], sum_wf['rmses']
-    e_rmse_nd, h_rmse_nd = sum_nd['epoch_rmse'], sum_nd['rmses']
-
-    fig, ax = plt.subplots(nrows=2, sharex=True, figsize=(8, 6))
-    ax[0].semilogy(e_loss_wf, h_loss_wf[0]/h_loss_wf[0,0], 'r-', label='Weak Form')
-    ax[0].semilogy(e_loss_nd, h_loss_nd[0]/h_loss_nd[0,0], 'b-', label='NODE')
-    ax[0].set_title('Training Loss (relative)')
-    ax[0].set_ylabel('Relative Loss')
-    ax[0].legend()
-
-    ax[1].semilogy(e_rmse_wf, h_rmse_wf[0], 'r-', label='Weak Form, Train')
-    ax[1].semilogy(e_rmse_wf, h_rmse_wf[2], 'r--', label='Weak Form, Test')
-    ax[1].semilogy(e_rmse_nd, h_rmse_nd[0], 'b-', label='NODE, Train')
-    ax[1].semilogy(e_rmse_nd, h_rmse_nd[2], 'b--', label='NODE, Test')
-    ax[1].set_title('Traj RMSE')
-    ax[1].set_xlabel('Epoch')
-    ax[1].set_ylabel('RMSE')
-    ax[1].legend()
-
-    print("Epoch time NODE/WF:", sum_nd['avg_epoch_time']/sum_wf['avg_epoch_time'])
+    print("Epoch time NODE/WF:", npzs[0]['avg_epoch_time']/npzs[1]['avg_epoch_time'])
 
 if ifprd:
     mdl_wf, prd_wf = load_model(MDL, f'lti_{mdl}_wf.pt', f'lti_{mdl}_wf.yaml')
@@ -108,10 +87,8 @@ if ifprd:
     u_data = us[0]
 
     with torch.no_grad():
-        _data = DynData(x_data, u_data)
-        weak_pred = prd_wf(x_data, _data, t_data)
-        _data = DynData(x_data, u_data)
-        node_pred = prd_nd(x_data, _data, t_data)
+        weak_pred = prd_wf(x_data, u_data, t_data)
+        node_pred = prd_nd(x_data, u_data, t_data)
 
     plot_trajectory(
         np.array([x_data, weak_pred, node_pred]), t_data, "LTI", metadata={'n_state_features': 2},
