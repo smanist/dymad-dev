9d0afb61bf198f59759095725db2c21ad00f0af9
Daning Huang <huangdn.nuaa@gmail.com>
2025-08-18 12:15:04 -0400

    Dependency and style cleaning for RALS

--- src/dymad/sako/rals.py
+++ src/dymad/sako/rals.py
@@ -1,51 +1,27 @@
-"""
-@package rals
-
-Resolvent Analysis for Linear Systems.
-
-For continuous-time system,
-    \dot{x} = Ax+Bu
-Resolvent operator H(s) = (sI-A)^{-1} B
-
-Following cases are considered:
-(1) Low-dimensional full-order systems
-(1.1) Regular resolvent
-    argmax || H(s)u || / ||u||, H(s) = (sI-A)^{-1} B
-
-(1.2) Constrained resolvent, where input & output are in subspace U
-    argmax || UH_r(s)a || / ||Ua||, H_r(s) = (sI-PAU)^{-1}
-    where PU=I_r.  B is ignored.
-    An alternative view for constrained resolvent is to think as
-    argmax || YH(s)Ua || / ||Ua||
-         = || Q^HH(s)Qg || / ||g||, U=QR, Ra=g
-    where Y=QQ^H is the projector onto U.
-
-(2) High-dimensional low-rank systems, where
-    A = UTV^H, with V^H U = I_r
-    The formulation is essentially the constrained resolvent,
-    with subspace U,
-    argmax || UH_r(s)a || / ||Ua||, H_r(s) = (sI-T)^{-1}
-         = || RH_r(s)R^{-1}g || / ||g||, U=QR, Ra=g
-
-@author Dr. Daning Huang
-@date 11/18/22
-"""
+import logging
 import numpy as np
 import scipy.linalg as spl
+from typing import Callable
 
-def resolventAnalysis(z, return_vec=False, A=None, B=None, U=None, ord=1):
+logger = logging.getLogger(__name__)
+
+def resolvent_analysis(
+        z: np.ndarray, return_vec: bool = False,
+        A: np.ndarray = None, B: np.ndarray = None, U: np.ndarray = None,
+        ord: int = 1):
     """
     Standalone, naive implementation of resolvent analysis, mainly for
     sanity check and suitable for small-scale problems.
     Uses SVD to compute resolvent of `A` at a given point `z`,
     and return the first `ord` modes and gains.
 
-    @param z: Point at which to compute resolvent
-    @param return_vec: If return I/O modes
-    @param A: The linear operator
-    @param B: The input matrix, only used if U is None
-    @param U: The constraining subspace
-    @param ord: Number of modes and gains to return
+    Args:
+        z (np.ndarray): Point at which to compute resolvent
+        return_vec (bool): If return I/O modes
+        A (np.ndarray): The linear operator
+        B (np.ndarray): The input matrix, only used if U is None
+        U (np.ndarray): The constraining subspace
+        ord (int): Number of modes and gains to return
     """
     if U is None:
         # No constraints on input/output space
@@ -79,22 +55,24 @@ def resolventAnalysis(z, return_vec=False, A=None, B=None, U=None, ord=1):
         return _s[:ord], np.squeeze(_U[:,:ord]), np.squeeze(_V[:,:ord])
     return _s[:ord]
 
-def estimatePSpec(grid, estimator, verbose=True, return_vec=False, **kwargs):
+def estimate_pseudospectrum(grid: np.ndarray, estimator: Callable, return_vec: bool = False, **kwargs):
     """
     Estimate pseudospectrum over a grid.
 
-    @param grid: List of points on complex plane
-    @param estimator: Function to evaluate gain and I/O modes at a point
-                      Args: complex point, return_vec, **kwargs
-    @param verbose: Whether to print info.
-    @param return_vec: If return I/O modes
-    @param kwargs: Args for estimator
+    Args:
+        grid (np.ndarray): List of points on complex plane
+        estimator (Callable): Function to evaluate gain and I/O modes at a point
+                              Args: complex point, return_vec, **kwargs
+        verbose (bool): Whether to print info.
+        return_vec (bool): If return I/O modes
+        kwargs: Args for estimator
     """
     _Ng = len(grid)
     kwargs.update(return_vec=return_vec)
 
     _es, _us, _vs = [], [], []
-    for _i, _z in enumerate(tqdm(grid)):
+    for _i, _z in enumerate(grid):
+        logger.info(f"Estimating pseudospectrum at point {_i+1}/{_Ng}: {_z}")
         _res = estimator(_z, **kwargs)
         if return_vec:
             _es.append(_res[0])
@@ -106,7 +84,7 @@ def estimatePSpec(grid, estimator, verbose=True, return_vec=False, **kwargs):
 
     _m = np.isfinite(_es)
     if not np.all(_m):
-        print("    Found inf gain(s) in pseudospectrum")
+        logger.info("Found inf gain(s) in pseudospectrum")
         _es[~_m] = np.max(_es)
 
     if return_vec:
@@ -118,18 +96,41 @@ def estimatePSpec(grid, estimator, verbose=True, return_vec=False, **kwargs):
 class RALowRank:
     """
     Resolvent analysis for low-rank linear systems
+
+    For continuous-time system,
+        \dot{x} = Ax+Bu
+    Resolvent operator H(s) = (sI-A)^{-1} B
+
+    Following cases are considered:
+    (1) Low-dimensional full-order systems
+    (1.1) Regular resolvent
+        argmax || H(s)u || / ||u||, H(s) = (sI-A)^{-1} B
+
+    (1.2) Constrained resolvent, where input & output are in subspace U
+        argmax || UH_r(s)a || / ||Ua||, H_r(s) = (sI-PAU)^{-1}
+        where PU=I_r.  B is ignored.
+        An alternative view for constrained resolvent is to think as
+        argmax || YH(s)Ua || / ||Ua||
+            = || Q^HH(s)Qg || / ||g||, U=QR, Ra=g
+        where Y=QQ^H is the projector onto U.
+
+    (2) High-dimensional low-rank systems, where
+        A = UTV^H, with V^H U = I_r
+        The formulation is essentially the constrained resolvent,
+        with subspace U,
+        argmax || UH_r(s)a || / ||Ua||, H_r(s) = (sI-T)^{-1}
+            = || RH_r(s)R^{-1}g || / ||g||, U=QR, Ra=g
+
+    In mode='disc', discrete-time matrix will be used
+    Ad = U exp(T*dt) V^H
+
+    Args:
+        U (np.ndarray): Left orthogonal factor of A
+        T (np.ndarray): Eigen block of A
+        V (np.ndarray): Right orthogonal factor of A
+        dt (float): Time step for discrete-time systems, default is 1.0
     """
-    def __init__(self, U, T, V, dt=1.0, verbose=True):
-        """
-        Initialize the instance.
-
-        A = U T V^H, V^H U = I_r
-
-        In mode='disc', discrete-time matrix will be used
-        Ad = U exp(T*dt) V^H
-
-        @param verbose: If print out processing information.
-        """
+    def __init__(self, U: np.ndarray, T: np.ndarray, V: np.ndarray, dt: float = 1.0):
         self._U = U
         self._T = T
         self._V = V
@@ -140,21 +141,22 @@ class RALowRank:
         self._Q, self._R = np.linalg.qr(self._U)
         self._Ir = np.eye(self._Nrnk)
 
-    def __call__(self, z, return_vec, mode='cont'):
+    def __call__(self, z: np.ndarray, return_vec: bool, mode: str = 'cont'):
         """
         Compute the resolvent norm at given complex point for pseudospectrum.
         Only the dominant one is returned.
 
-        @param z: The point at which to evaluate the resolvent norm
-        @param return_vec: If return resolvent modes
-        @param mode: 'cont' for c-t, 'disc' for d-t
+        Args:
+            z (np.ndarray): The point at which to evaluate the resolvent norm
+            return_vec (bool): If return resolvent modes
+            mode (str): 'cont' for c-t, 'disc' for d-t
         """
         _L = self._T if mode == 'cont' else self._Td
         _tmp = self._R.dot(z*self._Ir-_L).T
         try:
             _M = spl.solve(_tmp, self._R.T).T
         except np.linalg.LinAlgError:
-            print("    Singular matrix encountered; using PInv")
+            logger.info("RALowRank: Singular matrix encountered; using PInv")
             _M = spl.pinv(_tmp).dot(self._R.T).T
         # Since Nrnk is expected to be low, we just compute the entire set of SVD
         if return_vec:
